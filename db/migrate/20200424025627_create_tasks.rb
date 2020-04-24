b09d6d71423cd1547713eb5c0a26f510b893406d
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-04-24 13:24:51 +0900

    Taskマイグレーションファイル作成完了

---
+++ db/migrate/20200424025627_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text   :content
+      t.string :dead_line
+      t.string :condition
+      t.string :priority
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
