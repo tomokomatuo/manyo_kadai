79d5a52c1a218554350ce2a15b92f0a7d245bd9d
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-04-28 11:33:17 +0900

    i18n実装完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: "タスクを作成しました！"
+      redirect_to tasks_path, notice: t('view.create_task')
     else
       render :new
     end
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to tasks_path, notice: "タスクを更新しました！"
+      redirect_to tasks_path, notice: t('view.update_task')
     else
       render :edit
     end
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_path, notice: "タスクを削除しました！"
+    redirect_to tasks_path, notice: t('view.delete_task')
   end
 
   private
