b2c51a7906be5abfc1c234b6f8bcc9206ff17d0b
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-05-08 14:12:34 +0900

    自分以外のユーザーマイページにアクセスできない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        redirect_to user_path(@user.id)
-      else
-        render :new
-      end
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
   end
 
   def show
     @user = User.find(params[:id])
+    if logged_in?
+     if current_user.id != @user.id
+       redirect_to tasks_path
+     end
+    end
   end
       
   private
