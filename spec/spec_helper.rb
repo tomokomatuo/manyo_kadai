0689b676121a015a2a5f16c393898494ccc496f3
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-05-17 12:01:23 +0900

    ラベル機能rspecテスト完了

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,19 +13,24 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
 RSpec.configure do |config|
- 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
+  # ブラウザの表示設定
+  config.before(:each, type: :system) do
+    # driven_by :selenium_chrome_headless
+    driven_by :selenium_chrome
   end
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :truncation
+  # end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -61,14 +66,14 @@ RSpec.configure do |config|
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
-=begin
+
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
   # aliases for `it`, `describe`, and `context` that include `:focus`
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   config.filter_run_when_matching :focus
-
+=begin
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,28 +3,26 @@ require 'rails_helper'
 
 RSpec.describe 'タスク管理機能', type: :system do
   before do
-    @date = Date.new(2019, 9, 29)
-    @second_date = Date.new(2019, 10, 10)
-    # @label = FactoryBot.create(:label)
+    @date = Date.new(2020, 06, 20)
+    @second_date = Date.new(2020, 10, 10)
+    @label = FactoryBot.create(:label)
     # @second_label = FactoryBot.create(:second_label)
     @user = FactoryBot.create(:user)
-      # @second_user = FactoryBot.create(:second_user)
+    @second_user = FactoryBot.create(:second_user)
     @task = FactoryBot.create(:task, user: @user, dead_line: @date, priority: '低', condition: '完了')
-    # @second_task = FactoryBot.create(:second_task, user: @user, dead_line: @second_date, priority: '高', condition: '未着手')
+    @second_task = FactoryBot.create(:second_task, user: @user, dead_line: @second_date, priority: '高', condition: '未着手')
     # @labelling = FactoryBot.create(:labelling, task: @task, label: @label)
     
     visit new_session_path
-    expect(page).to have_content "Log in"
     fill_in 'session_email', with: 'sample@example.com'
     fill_in 'session_password', with: '00000000'
     click_on 'commit_new'
-    expect(page).to have_content "#{@user.name}のページ"
+    visit tasks_path
    end
   
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
-      visit tasks_path
       expect(page).to have_content 'task'
       end
     end
@@ -35,7 +33,11 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'title_new', with: 'task'
         fill_in 'content_new', with: 'content'
-        fill_in 'date_new', with: @date
+        select("2020", :from => 'task[dead_line(1i)]')
+        select("6月", :from => 'task[dead_line(2i)]')
+        select("20", :from => 'task[dead_line(3i)]')
+        # fill_in 'date_new', with: '6月'
+        # fill_in 'date_new', with: '20日'
         select('未着手', :from => 'task_condition')
         select('高', :from => 'task_priority')
         click_on '登録する'
@@ -46,7 +48,9 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'title_new', with: 'task'
         fill_in 'content_new', with: 'content'
-        fill_in 'date_new', with: @date
+        select("2020", :from => 'task[dead_line(1i)]')
+        select("6月", :from => 'task[dead_line(2i)]')
+        select("20", :from => 'task[dead_line(3i)]')
         select('未着手', :from => 'task_condition')
         select('高', :from => 'task_priority')
         check 'sample0'
@@ -59,14 +63,35 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク詳細画面' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移する' do
-        click_on "show_link"
-        expect(page).to have_content 'test_content'
+       
+        page.all(".show_link")[1].click
+        # visit task_path(@task)
+        expect(page).to have_content 'content'
        end
        it 'タスクに紐づいたラベルが表示される' do
-        click_on "show_link"
+       
+        page.all(".show_link")[1].click
         expect(page).to have_content 'sample0'
        end
      end
+  end
+  describe 'タスク編集画面' do
+    context '任意のタスク編集画面に遷移した場合' do
+      fit 'タスクが更新できる' do
+        visit edit_task_path(@task)
+        fill_in 'title_new', with: 'task'
+        fill_in 'content_new', with: 'content'
+        select("2020", :from => 'task[dead_line(1i)]')
+        select("6月", :from => 'task[dead_line(2i)]')
+        select("20", :from => 'task[dead_line(3i)]')
+        select('未着手', :from => 'task_condition')
+        select('高', :from => 'task_priority')
+        check 'sample0'
+        click_on '更新する'
+        save_and_open_page
+        expect(page).to have_content 'sample0'
+      end
+    end
   end
   describe 'タスク一覧画面' do
      context '検索をした場合' do
@@ -85,6 +110,11 @@ RSpec.describe 'タスク管理機能', type: :system do
         select('未着手', :from => 'task_condition')
         click_on 'submit'
         expect(page).to have_content 'task', '未着手'
+      end
+      it "ラベルで検索できる" do
+        select('sample0', :from => 'label_id')
+        click_on 'label_submit'
+        expect(page).to have_content 'sample0'
       end
     end
     context '複数のタスクを作成した場合' do
